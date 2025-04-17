f4e5ed8b07e0ded9bf36bdc7667d235dc2d87ef0
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2025-04-17 20:49:19 -0300

    adicionando buid.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+ECR_REGISTRY="773576874130.dkr.ecr.us-east-1.amazonaws.com"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
+docker build -t aws_aula .
+docker tag aws_aula:latest $ECR_REGISTRY/aws_aula:latest
+docker push $ECR_REGISTRY/aws_aula:latest
+ß
